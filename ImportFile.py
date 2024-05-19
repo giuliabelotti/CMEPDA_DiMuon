77a927a39362983c19d33fd156935a498e31e635
giuliabelotti <giulia.belotti@gmail.com>
2024-05-19 10:47:28 +0200

    RDataFrame

--- ImportFile.py
+++ ImportFile.py
@@ -7,11 +7,16 @@ def CreateDF():
         Returns:
             The RDataFrames        
     """
-    df_data_mu = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleMuParked.root")
+    df_data_mu = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/Run2012C_DoubleMuParked_merged.root")
+    
+    #df_data_mu = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleMuParked.root")
     #df_data_mu = ROOT.RDataFrame("Events", "http://opendata.cern.ch/eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/Run2011B_DoubleMu_merged.root")
         
         
-    df_data_el = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleElectron.root")
+    #df_data_el = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleElectron.root")
+    
+    df_data_el = ROOT.RDataFrame("Events", "http://opendata.cern.ch/eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/Run2012C_DoubleElectron_merged.root")
+       
     #df_data_el = ROOT.RDataFrame("Events", "http://opendata.cern.ch/eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/Run2011B_DoubleElectron_merged.root")
     
     df_MC = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/opendata/cms/derived-data/NanoAODRun1/01-Jul-22/MonteCarlo11_Summer11LegDR_DYJetsToLL_M-50_7TeV-madgraph-pythia6-tauola_merged.root")
