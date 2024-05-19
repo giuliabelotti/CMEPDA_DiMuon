486a87a30003e29b5977aef32b85fdf82cf0fff2
giuliabelotti <giulia.belotti@gmail.com>
2024-05-19 19:23:01 +0200

    Add cpp library

--- AngleDistribution.py
+++ AngleDistribution.py
@@ -18,112 +18,35 @@ def AngleDistribution(file_name, Tree_name, particle):
     """
     
     df = ROOT.RDataFrame(Tree_name, file_name)
-    
-    cpp_code_rapidity = '''   
-        float Rapidity(float pt1, float eta1, float phi1, float pt2, float eta2, float phi2, float mass){
-            TLorentzVector p1, p2;
-            p1.SetPtEtaPhiM(pt1, eta1, phi1, mass);
-            p2.SetPtEtaPhiM(pt2, eta2, phi2, mass);
-            return (p1+p2).Rapidity();
-        } 
-    '''
-    
-    cpp_code_pz = '''   
-        float pz(float pt1, float eta1, float phi1, float pt2, float eta2, float phi2, float mass){
-
-            TLorentzVector p1, p2;
-            p1.SetPtEtaPhiM(pt1, eta1, phi1, mass);
-            p2.SetPtEtaPhiM(pt2, eta2, phi2, mass);
-            return (p1+p2).Pz();
-        } 
-    '''
-    
-    cpp_code_P1p = '''   
-        float P1p(float pt1, float eta1, float phi1, float mass){
-            TLorentzVector p1;
-            p1.SetPtEtaPhiM(pt1, eta1, phi1, mass);
-            return (p1.E() + p1.Pz())/sqrt(2);
-        } 
-    '''
-    
-    cpp_code_P2p = '''   
-        float P2p(float pt2, float eta2, float phi2, float mass){
-            TLorentzVector p2;
-            p2.SetPtEtaPhiM(pt2, eta2, phi2, mass);
-            return (p2.E() + p2.Pz())/sqrt(2);
-        } 
-        
-    '''
-    
-    cpp_code_P1m = '''   
-        float P1m(float pt1, float eta1, float phi1, float mass){
-            TLorentzVector p1;
-            p1.SetPtEtaPhiM(pt1, eta1, phi1, mass);
-            return (p1.E() - p1.Pz())/sqrt(2);
-        } 
-        
-    '''
-    
-    cpp_code_P2m = '''   
-        float P2m(float pt2, float eta2, float phi2, float mass){
-            TLorentzVector p2;
-            p2.SetPtEtaPhiM(pt2, eta2, phi2, mass);
-            return (p2.E() - p2.Pz())/sqrt(2);
-        } 
-        
-    ''' 
-    
-    cpp_code_mass = '''   
-        float SystemMass(float pt1, float eta1, float phi1, float pt2, float eta2, float phi2, float mass){
-            TLorentzVector p1, p2;
-            p1.SetPtEtaPhiM(pt1, eta1, phi1, mass);
-            p2.SetPtEtaPhiM(pt2, eta2, phi2, mass);
-            return (p1+p2).M();
-        } 
-        
-    ''' 
-    
-    cpp_code_pT = '''   
-        float SystempT(float pt1, float eta1, float phi1, float pt2, float eta2, float phi2, float mass){
-            TLorentzVector p1, p2;
-            p1.SetPtEtaPhiM(pt1, eta1, phi1, mass);
-            p2.SetPtEtaPhiM(pt2, eta2, phi2, mass);
-            return (p1+p2).Pt();
-        }             
-    ''' 
-            
-    ROOT.gInterpreter.ProcessLine(cpp_code_rapidity)
-    ROOT.gInterpreter.ProcessLine(cpp_code_pz)
-    ROOT.gInterpreter.ProcessLine(cpp_code_P1p)
-    ROOT.gInterpreter.ProcessLine(cpp_code_P1m)
-    ROOT.gInterpreter.ProcessLine(cpp_code_P2p)
-    ROOT.gInterpreter.ProcessLine(cpp_code_P2m)
-    ROOT.gInterpreter.ProcessLine(cpp_code_mass)
-    ROOT.gInterpreter.ProcessLine(cpp_code_pT)
-    
+    ROOT.gInterpreter.ProcessLine('#include "Vector_Library.h"')
+  
     
     if(particle == 'Muon'):
-        df = df.Define("y", "Rapidity(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[0])")   
-        df = df.Define("pZ", "pz(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[0])")
-        df = df.Define("P1p", "P1p(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_mass[0])")
-        df = df.Define("P1m", "P1m(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_mass[0])")
-        df = df.Define("P2p", "P2p(Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[1])")
-        df = df.Define("P2m", "P2m(Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[1])")
-        df = df.Define("SystemMass", "SystemMass(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[0])")
-        df = df.Define("SystempT", "SystempT(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[0])")
-        df = df.Define("CosThetaMu", "(2*(P1p*P2m - P1m*P2p)*pZ)/(sqrt(pow(SystemMass,2)*(pow(SystemMass,2) + pow(SystempT,2)))*abs(pZ))")
+        df = df.Define("p1","Vector(Muon_pt[0], Muon_eta[0], Muon_phi[0], Muon_mass[0])")
+        df = df.Define("p2","Vector(Muon_pt[1], Muon_eta[1], Muon_phi[1], Muon_mass[1])")
+        df = df.Define("y", "Rapidity(p1,p2)")   
+        df = df.Define("pZ", "pz(p1,p2)")
+        df = df.Define("P1p", "P1p(p1)")
+        df = df.Define("P1m", "P1m(p1)")
+        df = df.Define("P2p", "P2p(p2)")
+        df = df.Define("P2m", "P2m(p2)")
+        df = df.Define("SystemMass", "invMass(p1,p2)")
+        df = df.Define("SystempT", "SystempT(p1,p2)")
+        df = df.Define("CosThetaMu", "CosTheta(SystemMass, pZ, P1p, P2p, P1m, P2m, SystempT)")
         
     
     elif(particle == 'Electron'):
-        df = df.Define("y", "Rapidity(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[0])")    
-        df = df.Define("pZ", "pz(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[0])")
-        df = df.Define("P1p", "P1p(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_mass[0])")
-        df = df.Define("P1m", "P1m(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_mass[0])")
-        df = df.Define("P2p", "P2p(Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[1])")
-        df = df.Define("P2m", "P2m(Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[1])")
-        df = df.Define("SystemMass", "SystemMass(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[0])")
-        df = df.Define("SystempT", "SystempT(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[0])")
-        df = df.Define("CosThetaEl", "(2*(P1p*P2m - P1m*P2p)*pZ)/(sqrt(pow(SystemMass,2)*(pow(SystemMass,2) + pow(SystempT,2)))*abs(pZ))")
+        df = df.Define("p1","Vector(Electron_pt[0], Electron_eta[0], Electron_phi[0], Electron_mass[0])")
+        df = df.Define("p2","Vector(Electron_pt[1], Electron_eta[1], Electron_phi[1], Electron_mass[1])") 
+        df = df.Define("y", "Rapidity(p1,p2)")    
+        df = df.Define("pZ", "pz(p1,p2)")
+        df = df.Define("P1p", "P1p(p1)")
+        df = df.Define("P1m", "P1m(p1)")
+        df = df.Define("P2p", "P2p(p2)")
+        df = df.Define("P2m", "P2m(p2)")
+        df = df.Define("SystemMass", "invMass(p1,p2)")
+        df = df.Define("SystempT", "SystempT(p1,p2)")
+        df = df.Define("CosThetaEl", "CosTheta(SystemMass, pZ, P1p, P2p, P1m, P2m, SystempT)")
         
     df_0_y_04 = df.Filter("abs(y)>0 && abs(y)<0.4", "Range Rapidity")
     df_08_y_12 = df.Filter("abs(y)>0.8 && abs(y)<1.2", "Range Rapidity")
