c2664857eadb2f289e806159d47f6b7d75474023
giuliabelotti <giulia.belotti@gmail.com>
2024-05-26 11:40:25 +0200

    Test Selection Mu

--- Test/Test_SelectionMu.py
+++ Test/Test_SelectionMu.py
@@ -12,12 +12,11 @@ class TestSelection(unittest.TestCase):
         tree = f.TreeMu
              
         for entry in tree:
-            self.assertEqual(entry.nMuon, 2, 'Exactly two muon')
-            self.assertTrue(entry.Muon_pt[0]>25 or entry.Muon_pt[1]>25, "Trigger")
-            self.assertTrue((entry.Muon_pt[0] > 25 and entry.Muon_pt[1] > 15) or (entry.Muon_pt[1] > 25 and entry.Muon_pt[0] > 15), "pT Cuts")
-            self.assertTrue(abs(entry.Muon_eta[0]) < 2.4 and abs(entry.Muon_eta[1]) < 2.4, "pT Cuts")
+           
+            self.assertTrue(((entry.GoodMuon_pt[0] > 15) and (entry.GoodMuon_pt[1] > 25)) or ((entry.GoodMuon_pt[1] > 15) and (entry.GoodMuon_pt[0] > 25)) , "pT Cuts")
             
-        
+            self.assertTrue(abs(entry.GoodMuon_eta[0]) < 2.4 and abs(entry.GoodMuon_eta[1]) < 2.4, "Eta Cuts")
+                             
 
 if __name__ == '__main__':
     unittest.main()
