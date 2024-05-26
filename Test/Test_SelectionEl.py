b803f4d3e4cee2a9044126e4bcb79b0a1b7a5354
giuliabelotti <giulia.belotti@gmail.com>
2024-05-26 11:40:44 +0200

    Test Selection El

--- Test/Test_SelectionEl.py
+++ Test/Test_SelectionEl.py
@@ -2,22 +2,21 @@
 import unittest
 import ROOT
 
-from ParticleSelection import MuCandidates
+from ParticleSelection import ElectronCandidates
 
 class TestSelection(unittest.TestCase):
     
-    def test_ElCandidates(self):
+    def test_ElectronCandidates(self):
        
         f = ROOT.TFile.Open("data/GoodElectron.root")
         tree = f.TreeEl
              
         for entry in tree:
-            self.assertEqual(entry.nElectron, 2, 'Exactly two electron')
-            self.assertTrue(entry.Electron_pt[0]>30 or entry.Electron_pt[1]>30, "Trigger")
-            self.assertTrue((entry.Electron_pt[0] > 30 and entry.Electron_pt[1] > 20) or (entry.Electron_pt[1] > 30 and entry.Electron_pt[0] > 20), "pT Cuts")
-            self.assertTrue(abs(entry.Electron_eta[0]) < 2.4 and abs(entry.Electron_eta[1]) < 2.4, "pT Cuts")
+           
+            self.assertTrue(((entry.GoodElectron_pt[0] > 20) and (entry.GoodElectron_pt[1] > 30)) or ((entry.GoodElectron_pt[1] > 20) and (entry.GoodElectron_pt[0] > 30)) , "pT Cuts")
             
-        
+            self.assertTrue(abs(entry.GoodElectron_eta[0]) < 2.4 and abs(entry.GoodElectron_eta[1]) < 2.4, "Eta Cuts")
+                             
 
 if __name__ == '__main__':
     unittest.main()
